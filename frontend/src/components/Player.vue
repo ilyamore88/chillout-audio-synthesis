75a4e1ae71973c4a45cc0bfb12cd870c85d57826
ilyamore88 <themorozik@gmail.com>
2019-10-12 21:28:20 +0300

    fix for safari

--- frontend/src/components/Player.vue
+++ frontend/src/components/Player.vue
@@ -50,7 +50,7 @@
     name: 'Player',
     data() {
       return {
-        audioCtx: new (AudioContext || window.webkitAudioContext)(),
+        audioCtx: null,
         currentInstrument: null,
         gainValue: 0.35,
         noteList: 'E4 G4 A4 B4 G4 B4 B4 A4 A4 G4 G4 E4 E4',
@@ -60,9 +60,23 @@
       };
     },
     mounted() {
-      this.currentInstrument = new Instrument(this.audioCtx);
-      this.currentInstrument.setGainValue(this.gainValue);
-      this.currentInstrument.setWave(this.waveType);
+      const AudioContext = window.AudioContext || // Default
+        window.webkitAudioContext || // Safari and old versions of Chrome
+        false;
+
+      if (AudioContext) {
+        // Do whatever you want using the Web Audio API
+        this.audioCtx = new AudioContext();
+        this.currentInstrument = new Instrument(this.audioCtx);
+        this.currentInstrument.setGainValue(this.gainValue);
+        this.currentInstrument.setWave(this.waveType);
+      } else {
+        /*
+         * Web Audio API is not supported
+         * Alert the user
+         */
+        alert('Sorry, but the Web Audio API is not supported by your browser. Please, consider upgrading to the latest version or downloading Google Chrome or Mozilla Firefox');
+      }
     },
     updated() {
       if (this.currentInstrument) {
